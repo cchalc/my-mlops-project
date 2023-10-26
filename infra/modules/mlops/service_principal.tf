7e621eb0b2e59714260d60b0fd8b9c56185663b9
Christopher Chalcraft <chris.chalcraft@gmail.com>
2023-10-26 15:20:55 -0700

    base mlops-stacks

---
+++ infra/modules/mlops/service_principal.tf
@@ -0,0 +1,5 @@
+module "mlops-azure-project-with-sp-linking" {
+  source  = "databricks/mlops-azure-project-with-sp-linking/databricks"
+  version = "0.0.3"
+  # insert the 4 required variables here
+}
